af338c2f26473fc9309bedf7d1cd3a2671a8a02b
Kostya <kostya.pershin007@gmail.com>
2022-04-28 21:04:20 +0400

    update

--- controller/employee.controller.js
+++ controller/employee.controller.js
@@ -4,12 +4,11 @@ class Employee {
 	async createEmployee(req, res) {
 		const {
 			name,
-			surname,
 			position,
 			teamId
 		} = req.body;
 
-		const newEmployee = await db.query(`INSERT INTO employee (name,surname,position) values ($1, $2, $3) RETURNING *`, [name, surname, position]);
+		const newEmployee = await db.query(`INSERT INTO employee (name,position) values ($1, $2) RETURNING *`, [name, position]);
 
 		res.send({ newEmployee: newEmployee.rows[0] });
 	}
