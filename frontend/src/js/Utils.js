9e268b7b88b92fa54956be0b650b7657e58112f9
Kostya <kostya.pershin007@gmail.com>
2022-04-29 11:06:53 +0400

    fix

--- frontend/src/js/Utils.js
+++ frontend/src/js/Utils.js
@@ -1,4 +1,4 @@
-export const getURL = () => '';
+export const getURL = () => process.env.NODE_ENV === "production" ? "" : 'http://localhost:3000';
 
 export function copyText(text) {
 	const input = document.createElement('input');
